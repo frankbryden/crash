b53cb8e30b853ff16476619f47e64c9449cdd2bc
Theo Maurin <theo.maurin@gmail.com>
2024-12-07 22:11:00 +0100

    Added bidding, (not yet tracked) gain and fixed a couple bugs (including game never starting)
    Added cash
    
    Added cash updated after gain
    
    Implementing comments and changing bidding to only waiting phase

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from typing import Union
 import json
 from threading import Thread, Event
@@ -43,7 +43,7 @@ player_join_event = Event()
 async def loop():
     while True:
         current_players = game_handler.players
-
+        # print(current_players)
         # If there is no player we wait for one to save CPU cycles
         if len(current_players) == 0:
             print("Waiting for players...")
@@ -55,12 +55,12 @@ async def loop():
 
         elif game.is_waiting():
             await manager.broadcast_lobby({"type": "state", "state": "waiting"})
-            time.sleep(5)
+            time.sleep(1)
 
         if game.is_crashed():
             game.reset_game()
             print("Resetting game!")
-            # await manager.broadcast_lobby("Resetting game!")
+            await manager.broadcast_lobby({"type": "state", "state": "crashed"})
             game.reset_waiting_time()
 
         # Clear the event so that we can set it again
@@ -108,6 +108,12 @@ async def websocket_endpoint(websocket: WebSocket):
             message = json.loads(data)
 
             if message["type"] == "join":
+
+                # A player has joined an empty lobby
+                if len(current_players) == 0:
+                    player_join_event.set()
+                    game.reset_waiting_time()
+
                 name = message["name"]
                 game_handler.join(websocket, name)
                 await manager.broadcast_lobby(
@@ -117,38 +123,54 @@ async def websocket_endpoint(websocket: WebSocket):
                         "lobby": list(current_players.values()),
                     }
                 )
-                # A player has joined
-                player_join_event.set()
 
             elif message["type"] == "bid":
-                if not game.ongoing:
+                if game.ongoing or game.is_crashed():
                     await manager.send_personal_message_lobby(
-                        {"type": "error", "message": "Game has not started."}
+                        {"type": "error", "message": "Game is already going."},
+                        websocket,
                     )
                 else:
                     amount = message["amount"]
+                    game.bid(websocket, amount)
+                    # TODO: find a way to avoid recomputing the whole dicts each time a player makes a bid
+                    bids_dict = {}
+                    for ws in game.bids:
+                        bids_dict[current_players[ws]] = game.bids[ws]
+                    cash_dict = {}
+                    for ws in game_handler.cash:
+                        cash_dict[current_players[ws]] = game_handler.cash[ws]
                     await manager.broadcast_lobby(
                         {
                             "type": "bid",
                             "target": current_players[websocket],
                             "amount": amount,
+                            "bids": bids_dict,
+                            "cash_vaults": cash_dict,
                         }
                     )
 
             elif message["type"] == "cashout":
                 if not game.ongoing:
                     await manager.send_personal_message_lobby(
-                        {"type": "error", "message": "Game has not started."}
+                        {"type": "error", "message": "Game has not started."}, websocket
                     )
                 else:
+                    gains = game.cashout(websocket)
+                    for ws in game_handler.cash:
+                        cash_dict[current_players[ws]] = game_handler.cash[ws]
                     await manager.broadcast_lobby(
                         {
                             "type": "cashout",
                             "target": current_players[websocket],
                             "mult": game.get_multiplicator(),
+                            "gains": gains,
+                            "cash_vaults": cash_dict,
                         }
                     )
-    except:
+
+    except WebSocketDisconnect:
+
         manager.disconnect(websocket)
         new_player_list = list(current_players.values())
         new_player_list.remove(current_players[websocket])

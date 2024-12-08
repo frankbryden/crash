8ac0595fd87e3fa8b966b95417411d42be0d7b26
Choklatier <48896806+Choklatier@users.noreply.github.com>
2024-12-08 18:15:34 +0100

    workflow yaml and test skeleton (#9)
    * workflow yaml and test skeleton
    
    * Added first two connection tests

---
+++ backend/src/test/test_websockets.py
@@ -0,0 +1,56 @@
+import pytest
+from fastapi.testclient import TestClient
+from fastapi import WebSocket
+from crash.main import app  # Replace with your actual app import
+from typing import Callable
+
+client = TestClient(app)
+
+
+@pytest.fixture
+def name() -> str:
+    return "Franku"
+
+
+@pytest.fixture
+def websocket_connection() -> Callable[[], WebSocket]:
+    return lambda: client.websocket_connect("/ws")
+
+
+def join_message(name: str) -> dict:
+    return {"type": "join", "name": name}
+
+
+def test_join(name: str, websocket_connection: WebSocket):
+    with websocket_connection() as websocket:
+        # Send a message
+        websocket.send_json(join_message(name))
+
+        # Receive a response
+        data = websocket.receive_json()
+
+        # Assertions
+        assert data["type"] == "join"
+        assert data["target"] == name
+
+
+def test_two_players(name: str, websocket_connection: WebSocket):
+    name1 = name + "1"
+    name2 = name + "2"
+    with websocket_connection() as websocket:
+        with websocket_connection() as websocket2:
+            # Send a messages
+            websocket.send_json(join_message(name1))
+            websocket2.send_json(join_message(name2))
+
+            # Receive a response
+            # Discard initial response (tested in test_join)
+            websocket.receive_json()
+
+            # Should get lobby message triggered by 2nd connection
+            data = websocket.receive_json()
+
+            # Assertions
+            assert data["type"] == "join"
+            assert data["target"] == name2
+            assert data["lobby"] == [name1, name2]
